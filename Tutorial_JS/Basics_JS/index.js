529820c68269c3cfa8452eb5e519225e1439841b
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-12 09:41:27 -0500

    Learning Basic JS

---
+++ Tutorial_JS/Basics_JS/index.js
@@ -0,0 +1,13 @@
+// This appears on the console, when you do inspect
+console.log("I like pizza!");
+console.log("It's really good!");
+
+// This is a pop up on the pages
+window.alert("I REALLY LOVE PIZZA!");
+
+// And this is a comment
+
+/* 
+Same as CSS it has block comments
+Which are nice too.
+*/
