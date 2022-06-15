e9afd8a05b8280fc0693ea9203bbd1e05ff4c2c5
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-14 22:15:12 -0500

    Forgot this file

---
+++ Tutorial_JS/Math_JS_6/index.js
@@ -0,0 +1,14 @@
+// This is the math library
+
+// Here are some calls for this library
+
+// x = Math.round(x); round closer
+// x = Math.floor(x); round down
+// x = Math.ceil(x);  round up
+// x = Math.pow(x, 2);  to the power
+// x = Math.sqrt(x);  square root
+// x = Math.abs(x);  absolute value
+// maximum = Math.max(x, y, z); 
+// minimum = Math.min(x, y, z);
+
+x = Math.PI;
