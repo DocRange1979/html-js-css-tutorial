8a7f2ff5a2dc2a3af77c481e42674646e6a8edf7
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-15 21:53:07 -0500

    Basic web page for hypotenuse of a triangle with Pitagoras.

--- Tutorial_JS/Practice_JS_7/index.js
+++ Tutorial_JS/Practice_JS_7/index.js
@@ -1,7 +1,7 @@
 let a;
 let b;
 let c;
-
+/*
 a = window.prompt("Enter side a: ");
 a = Number(a);
 
@@ -10,3 +10,15 @@ b = Number(b);
 
 c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 console.log("Side C: ", c);
+*/
+
+document.getElementById("submitButton").onclick = function(){
+    a = document.getElementById("aTextBox").value;
+    a = Number(a);
+
+    b = document.getElementById("bTextBox").value;
+    b = Number(b);
+    
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    document.getElementById("cLabel").innerHTML = "Side C: " + c;
+}
