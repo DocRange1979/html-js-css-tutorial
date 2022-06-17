59a8047fb14682429c41bcd31bec33bbaa4b0be2
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-16 21:47:55 -0500

    Learning If Else statements on Javascript

---
+++ Tutorial_JS/if_else_JS_10/index.js
@@ -0,0 +1,15 @@
+// If else statements are esential for programming
+
+let age = 18;
+
+if(age >= 18){
+    console.log("You are an adult!");
+}
+
+else if(age < 0){
+    console.log("YOU HAVEN'T BEEN BORN YET");
+}
+
+else{
+    console.log("You are a child!");
+}
