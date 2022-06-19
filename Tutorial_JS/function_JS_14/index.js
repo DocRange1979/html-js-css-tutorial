2e685821af3280b8992e8575a2c3e5f1fb13a9ef
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-19 18:17:02 -0500

    Learning return on Javascript

--- Tutorial_JS/function_JS_14/index.js
+++ Tutorial_JS/function_JS_14/index.js
@@ -14,3 +14,21 @@ function happyBirthday(name, number){
     console.log("Happy birthday to you!");
     console.log("You are", number, " years old!");
 }
+
+// return statement
+
+let area;
+let width;
+let height;
+
+width = window.prompt("Enter width");
+height = window.prompt("Enter height");
+
+area = getArea(width, height);
+
+console.log("The area is:", area);
+
+function getArea(width, height){
+    let result = width * height;
+    return result;
+}
