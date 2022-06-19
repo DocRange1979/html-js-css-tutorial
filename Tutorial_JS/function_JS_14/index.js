9f3eb368f3fc8bd9a48ee3ab7508880def3ee32a
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-19 18:08:02 -0500

    Learning functions on Javascript

---
+++ Tutorial_JS/function_JS_14/index.js
@@ -0,0 +1,16 @@
+startProgram();
+
+function startProgram(){
+    let userName = "Bro";
+    let age = "21";
+
+    happyBirthday(userName, age);
+}
+
+function happyBirthday(name, number){
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear", name);
+    console.log("Happy birthday to you!");
+    console.log("You are", number, " years old!");
+}
