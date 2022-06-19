efb2224f7343d8764eccbe833ca4cd7500c45cbc
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-19 15:32:54 -0500

    Learning and or statements

---
+++ Tutorial_JS/and_or_JS_12/index.js
@@ -0,0 +1,19 @@
+// Classical and or statements for programming
+
+let temp = 15;
+
+if(temp > 0 && temp < 30){                   // And operator
+    console.log("The weather is good!");
+}
+else{
+    console.log("The weather is bad");
+}
+
+if(temp <= 0 || temp >= 30){
+    console.log("The weather is bad");
+}
+else{
+    console.log("The weather is good!");
+}
+
+// 
