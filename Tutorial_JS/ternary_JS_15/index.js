c44feaca697f2eb73c500f7ec83f7824c68b20b2
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-19 18:25:34 -0500

    Learning about ternary in Javascript

---
+++ Tutorial_JS/ternary_JS_15/index.js
@@ -0,0 +1,10 @@
+// Ternary operators
+
+// condition ? exprIfTrue : exprIfFalse
+
+let adult = checkAge(12);
+console.log(adult);
+
+function checkAge(age){
+    return age >= 18 ? true : false    // Like in this case
+}
