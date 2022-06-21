0480892e2283a3b3222991e11e0d0ec6fd2e4cf4
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-20 20:49:51 -0500

    Added new folder and learned var vs let in Javascript

---
+++ Tutorial_JS_2/variables_JS_1/index.js
@@ -0,0 +1,6 @@
+// let = variables are limited to block scope {}
+// var = variables are limited to a function() {}
+
+// global varibale = is declared outside any function
+// (if global, var will CHANGE browser's window properties)
+
