820fa7f367345d8c97a402985564890c0b481f41
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-20 20:57:01 -0500

    Leaning template literals on Javascript

--- Tutorial_JS_2/variables_JS_1/index.js
+++ Tutorial_JS_2/variables_JS_1/index.js
@@ -4,3 +4,15 @@
 // global varibale = is declared outside any function
 // (if global, var will CHANGE browser's window properties)
 
+// ----------------------
+
+// Template literals
+
+let userName = "Bro";
+let items = 3;
+let total = 75;
+
+console.log(`Hello ${userName}`);
+console.log(`You have ${items} items in your cart`);
+console.log(`Your total is $${total}`);
+
