ad730886e86e347d095023d27c0ec982c95a2af3
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-20 21:09:04 -0500

    Learning number formatting in Javascript

--- Tutorial_JS_2/variables_JS_1/index.js
+++ Tutorial_JS_2/variables_JS_1/index.js
@@ -16,3 +16,20 @@ console.log(`Hello ${userName}`);
 console.log(`You have ${items} items in your cart`);
 console.log(`Your total is $${total}`);
 
+// ---------------------------
+
+// format currency
+// number.toLocaleString(locale, {options});
+// 'locale' = specify that language (undefined = default set in browser)
+// 'options' = object with formatting options
+
+let myNum = 123456.789;
+
+myNum = myNum.toLocaleString("en-US"); // US English
+myNum = myNum.toLocateString("hi_IN"); // Hindi
+
+myNum = myNum.toLocaleString("en-US", {style: "currency", currency: "USD"});
+myNum = myNum.toLocateString("hi-IN", {style: "currency", currency: "INR"});
+
+myNum = myNum.toLocateString(undefined, {style: "unit", unit:"celsius"});
+console.log(myNum)
