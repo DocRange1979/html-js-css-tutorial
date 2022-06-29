1bbc3b4f24c2a9a457419fbaae69c7a1e4291d0a
DocRange1979 <edu.rojas.paez@gmail.com>
2022-06-28 20:40:38 -0500

    Loop through arrays on Javascript

--- Tutorial_JS_2/arrays_JS_2/index.js
+++ Tutorial_JS_2/arrays_JS_2/index.js
@@ -14,3 +14,15 @@ fruits.shift();  // Removes first element
 let length = fruits.length;  // Lenght of the array
 let index = fruits.indexOf("banana");  // Index of object, -1 if does not exists
 
+// Loop on arrays
+
+let prices = [5, 10, 15, 20];
+
+
+for(let i = 0; i < prices.length; i+=1){
+    console.log(prices[i]);
+}
+
+for(let price of prices){
+    console.log(price);
+}
